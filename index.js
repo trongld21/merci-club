bed711153dabc6909b06140d5725afacf5f14c6d
ducctrong <trongld21@gmail.com>
2022-07-11 23:27:42 +0700

    Init

---
+++ index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $('.our-clients-carousel').slick({
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+    });
+
+    $('.masterstudy-blog-carousel').slick({
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+    });
+    var btn = $('#buttonBackToTop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+});
